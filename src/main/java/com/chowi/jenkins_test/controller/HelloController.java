0751726c4f58d2d348270882b03a7c4afbb94de9
Guojingye <guojingye@banggood.com>
2019-06-13 11:56:34 +0800

    init repo

---
+++ src/main/java/com/chowi/jenkins_test/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.chowi.jenkins_test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    @RequestMapping(value = "say")
+    @ResponseBody
+    public String sayHello(){
+        return "Hi! Jenkins";
+    }
+}
